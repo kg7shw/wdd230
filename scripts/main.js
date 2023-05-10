e272fb81dc3c50398964288167f4b377342ad519
kg7shw <kg7shw@gmail.com>
2023-05-09 18:58:22 -0600

    added svg icons for menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
-const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
+// const hamButton = document.querySelector("#menu");
+// const navigation = document.querySelector(".navigation");
 
-hamButton.addEventListener("click", () => {
-  navigation.classList.toggle("open");
-  hamButton.classList.toggle("open");
+// hamButton.addEventListener("click", () => {
+//   navigation.classList.toggle("open");
+//   hamButton.classList.toggle("open");
+// });
+
+const hamMenuButton = document.querySelector("#ham-menu");
+const nav = document.querySelector(".nav");
+
+hamMenuButton.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  hamMenuButton.classList.toggle("open");
 });
