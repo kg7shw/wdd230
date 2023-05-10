51c0944c62adc907f76b87a699b8d70c3b61837b
kg7shw <kg7shw@gmail.com>
2023-05-09 22:08:48 -0600

    added dark theme to body and removed it from main

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ hamMenuButton.addEventListener("click", () => {
 // Theme button
 const themeBtn = document.querySelector("#theme-toggle");
 themeBtn.addEventListener("click", () => {
-  document.querySelector("main").classList.toggle("dark-theme");
+  document.querySelector("body").classList.toggle("dark-theme");
   themeBtn.querySelectorAll(".icon").forEach((item) => {
     item.classList.toggle("active");
   });
