1b8ab234f412bcf570b41713d017426127ef125a
kg7shw <kg7shw@gmail.com>
2023-06-01 12:34:37 -0600

    updated the larger view

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -9,12 +9,3 @@ hamMenuButton.addEventListener("click", () => {
     item.classList.toggle("active");
   });
 });
-
-// Theme button
-const themeBtn = document.querySelector("#theme-toggle");
-themeBtn.addEventListener("click", () => {
-  document.querySelector("body").classList.toggle("dark-theme");
-  themeBtn.querySelectorAll(".icon").forEach((item) => {
-    item.classList.toggle("active");
-  });
-});
