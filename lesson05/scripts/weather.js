69dfb046e8188322840639e543b7e9cbac3d4181
kg7shw <kg7shw@gmail.com>
2023-06-17 17:36:28 -0600

    fixed temp api retrieval

--- lesson05/scripts/weather.js
+++ lesson05/scripts/weather.js
@@ -5,10 +5,10 @@ const url =
   "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=49.75&lon=6.64&appid=17f9ad6c31f529f1ee759cfe8b2014d0";
 
 function displayResults(data) {
-  currentTemp.innerHTML = `${data.temp}&deg;F`;
-  const iconsrc = `https://openweathermap.org/img/wn/${data.weather.icon}.png`;
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
   let desc = data.weather[0].description;
-  weatherIcon.setAttribute(url);
+  weatherIcon.setAttribute("src", iconsrc);
   weatherIcon.setAttribute("alt", "test");
   captionDesc.textContent = `${desc}`;
 }
@@ -20,7 +20,7 @@ async function apiFetch() {
       const data = await response.json();
       console.log(data);
       //   displayData(data);
-      //   displayResults(data);
+      displayResults(data);
     } else {
       throw Error(await response.text());
     }
