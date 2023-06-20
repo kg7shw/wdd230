11d0f05c19c6e5f33faad2caa64628921868e3b6
kg7shw <kg7shw@gmail.com>
2023-06-20 08:28:03 -0600

    added dynamic weather input through api request

---
+++ scripts/weather-api.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=49.75&lon=6.64&appid=17f9ad6c31f529f1ee759cfe8b2014d0";
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", "test");
+  captionDesc.textContent = `${desc}`;
+}
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      //   displayData(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
