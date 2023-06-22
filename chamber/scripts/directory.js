2592c264b3dc0fc2b74c371ba442c6e21ea9b9d1
kg7shw <kg7shw@gmail.com>
2023-06-22 13:00:36 -0600

    updated the fetch request parameters

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -3,8 +3,17 @@ const membersURL = "https://kg7shw.github.io/wdd230/data/members.json";
 
 async function getMembers() {
   try {
-    const response = await fetch();
+    const response = await fetch(membersURL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      //   displayMembers(data);
+    } else {
+      throw Error(await response.text());
+    }
   } catch (error) {
     console.log(error);
   }
 }
+
+getMembers();
