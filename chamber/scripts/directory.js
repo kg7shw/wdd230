5a549d1d71c1b51481ebfc53ef2401a832af4ab6
kg7shw <kg7shw@gmail.com>
2023-06-22 12:51:41 -0600

    update

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,10 @@
+const baseURL = "https://kg7shw.github.io/wdd230";
+const membersURL = "https://kg7shw.github.io/wdd230/data/members.json";
+
+async function getMembers() {
+  try {
+    const response = await fetch();
+  } catch (error) {
+    console.log(error);
+  }
+}
