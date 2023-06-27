fe0b442e405a4dbadaab4517f01c91c9907c0013
kg7shw <kg7shw@gmail.com>
2023-06-27 08:29:58 -0600

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,6 +1,31 @@
 const baseURL = "https://kg7shw.github.io/wdd230";
 const membersURL = "https://kg7shw.github.io/wdd230/data/members.json";
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+  // example using arrow function
+  display.classList.add("grid");
+  display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+  display.classList.add("list");
+  display.classList.remove("grid");
+}
+
+function displayMembers(data) {
+  const article = document.querySelector(".directory-article");
+
+  data.members.forEach((member) => {
+    const section = document.createElement("section");
+  });
+}
+
 async function getMembers() {
   try {
     const response = await fetch(membersURL);
