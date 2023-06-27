be963d0667223bf737b1ebd93f816ae3bb497467
kg7shw <kg7shw@gmail.com>
2023-06-27 09:50:41 -0600

    added the card contents

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -3,27 +3,69 @@ const membersURL = "https://kg7shw.github.io/wdd230/data/members.json";
 
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+// const display = document.querySelector("article");
+
+const articleMainDisplay = document.querySelector(".directory-article");
 
 gridbutton.addEventListener("click", () => {
-  // example using arrow function
   display.classList.add("grid");
   display.classList.remove("list");
 });
 
-listbutton.addEventListener("click", showList); // example using defined function
+listbutton.addEventListener("click", showList);
 
 function showList() {
   display.classList.add("list");
   display.classList.remove("grid");
 }
 
-function displayMembers(data) {
-  const article = document.querySelector(".directory-article");
+// gridbutton.addEventListener("click", () => {
+//   // example using arrow function
+//   display.classList.add("grid");
+//   display.classList.remove("list");
+// });
 
-  data.members.forEach((member) => {
-    const section = document.createElement("section");
-  });
+// listbutton.addEventListener("click", showList); // example using defined function
+
+// function showList() {
+//   display.classList.add("list");
+//   display.classList.remove("grid");
+// }
+
+// function displayMembers(data) {
+//   const article = document.querySelector(".directory-article");
+
+//   data.members.forEach((member) => {
+//     const section = document.createElement("section");
+//     const img = document.createElement("img");
+//     const h3 = document.createElement("h3");
+//     const p = document.createElement("p");
+//   });
+// }
+
+function displayMembers(data) {
+  console.log(data);
+  const ul = document.createElement("ul");
+  ul.classList.add("member-list");
+
+  for (let member in data.members) {
+    console.log(member);
+    console.log(data[member]);
+    const li = document.createElement("li");
+    li.classList.add("member-list-item");
+    li.innerHTML = `
+    <img src="${data.members[member].image}" height="50">
+    <h3 class="name">${data.members[member].name}</h3>
+    <p class="address">${data.members[member].address}</p>
+    <p class="phone">${data.members[member].phone}</p>
+    <p class="website">${data.members[member].website}</p>
+
+    `;
+
+    ul.appendChild(li);
+  }
+
+  articleMainDisplay.appendChild(ul);
 }
 
 async function getMembers() {
@@ -32,7 +74,7 @@ async function getMembers() {
     if (response.ok) {
       const data = await response.json();
       console.log(data);
-      //   displayMembers(data);
+      displayMembers(data);
     } else {
       throw Error(await response.text());
     }
