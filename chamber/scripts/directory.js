5e0cd70805cb03f69dff753681e21996f3a72c05
kg7shw <kg7shw@gmail.com>
2023-06-27 19:19:34 -0600

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,72 +1,23 @@
 const baseURL = "https://kg7shw.github.io/wdd230";
-const membersURL = "https://kg7shw.github.io/wdd230/data/members.json";
+const membersURL = `${baseURL}/data/members.json`;
 
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-// const display = document.querySelector("article");
+// Move the gridButton, listButton, and memberList variables below their respective elements in the HTML
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+const memberList = document.querySelector("#member-list");
 
-const articleMainDisplay = document.querySelector(".directory-article");
-
-gridbutton.addEventListener("click", () => {
-  display.classList.add("grid");
-  display.classList.remove("list");
+gridButton.addEventListener("click", () => {
+  memberList.classList.add("grid");
+  memberList.classList.remove("list");
 });
 
-listbutton.addEventListener("click", showList);
+listButton.addEventListener("click", () => {
+  memberList.classList.add("list");
+  memberList.classList.remove("grid");
+});
 
-function showList() {
-  display.classList.add("list");
-  display.classList.remove("grid");
-}
-
-// gridbutton.addEventListener("click", () => {
-//   // example using arrow function
-//   display.classList.add("grid");
-//   display.classList.remove("list");
-// });
-
-// listbutton.addEventListener("click", showList); // example using defined function
-
-// function showList() {
-//   display.classList.add("list");
-//   display.classList.remove("grid");
-// }
-
-// function displayMembers(data) {
-//   const article = document.querySelector(".directory-article");
-
-//   data.members.forEach((member) => {
-//     const section = document.createElement("section");
-//     const img = document.createElement("img");
-//     const h3 = document.createElement("h3");
-//     const p = document.createElement("p");
-//   });
-// }
-
-function displayMembers(data) {
-  console.log(data);
-  const ul = document.createElement("ul");
-  ul.classList.add("member-list");
-
-  for (let member in data.members) {
-    console.log(member);
-    console.log(data[member]);
-    const li = document.createElement("li");
-    li.classList.add("member-list-item");
-    li.innerHTML = `
-    <img src="${data.members[member].image}" height="50">
-    <h3 class="name">${data.members[member].name}</h3>
-    <p class="address">${data.members[member].address}</p>
-    <p class="phone">${data.members[member].phone}</p>
-    <p class="website">${data.members[member].website}</p>
-
-    `;
-
-    ul.appendChild(li);
-  }
-
-  articleMainDisplay.appendChild(ul);
-}
+// Move the articleMainDisplay variable below the displayMembers function
+const articleMainDisplay = document.querySelector(".directory-article");
 
 async function getMembers() {
   try {
@@ -80,6 +31,61 @@ async function getMembers() {
     }
   } catch (error) {
     console.log(error);
+  }
+}
+
+function displayMembers(data) {
+  const ul = document.createElement("ul");
+  ul.setAttribute("id", "member-list");
+  ul.classList.add("member-list");
+
+  for (let member of data.members) {
+    const li = document.createElement("li");
+    li.classList.add("member-list-item");
+    li.innerHTML = `
+      <img src="${member.image}" height="50">
+      <h3 class="name">${member.name}</h3>
+      <p class="address">${member.address}</p>
+      <p class="phone">${member.phone}</p>
+      <p class="website">${member.website}</p>
+    `;
+
+    ul.appendChild(li);
+  }
+
+  articleMainDisplay.appendChild(ul);
+
+  // Add the showList function inside displayMembers to apply the event listener after rendering the members
+  function showList() {
+    const menu = document.getElementById("menu");
+    const list = document.getElementById("list");
+
+    if (menu && list) {
+      menu.classList.add("active");
+      list.classList.remove("hidden");
+    }
+  }
+
+  function hideList() {
+    const menu = document.getElementById("menu");
+    const list = document.getElementById("list");
+
+    if (menu && list) {
+      menu.classList.remove("active");
+      list.classList.add("hidden");
+    }
+  }
+
+  const menuButton = document.getElementById("menu-button");
+  if (menuButton) {
+    menuButton.addEventListener("click", function () {
+      const list = document.getElementById("list");
+      if (list && list.classList.contains("hidden")) {
+        showList();
+      } else {
+        hideList();
+      }
+    });
   }
 }
 
