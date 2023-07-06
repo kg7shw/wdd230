1d711a929d210bf535883497d1fb922a0217fcb7
kg7shw <kg7shw@gmail.com>
2023-07-06 10:31:12 -0600

    added five day forcast weather api

---
+++ chamber/scripts/weather-api-chamber.js
@@ -0,0 +1,46 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+// const lat = "43.82";
+// const lon = "-111.79";
+
+const lat = "16.042";
+const lon = "120.33";
+const units = "metric";
+const apiKey = "17f9ad6c31f529f1ee759cfe8b2014d0";
+
+// const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=43.82&lon=-111.79&appid=17f9ad6c31f529f1ee759cfe8b2014d0";
+const currentWeatherDataUrl = `https://api.openweathermap.org/data/2.5/weather?units=${units}&lat=${lat}&lon=${lon}&appid=${apiKey}`;
+// console.log(url);
+console.log(currentWeatherDataUrl);
+
+const fiveDayThreeHourForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+console.log(fiveDayThreeHourForecast);
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", "test");
+  captionDesc.textContent = `${desc}`;
+}
+
+async function apiFetch(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// apiFetch(currentWeatherDataUrl);
+// apiFetch(fiveDayThreeHourForecast);
--- scripts/weather-api.js
+++ scripts/weather-api.js
@@ -1,8 +1,16 @@
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
-const url =
+const lat = "43.82";
+const lon = "-111.79";
+const apiKey = "ce57d3e509bb08e5388443da788e875f";
+const GMurl =
   "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=49.75&lon=6.64&appid=17f9ad6c31f529f1ee759cfe8b2014d0";
+const rxUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=43.82&lon=-111.79&appid=17f9ad6c31f529f1ee759cfe8b2014d0";
+const url = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const currentWeatherDataUrl1 =
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&16.044&120.332&appid=ce57d3e509bb08e5388443da788e875f";
 
 function displayResults(data) {
   currentTemp.innerHTML = `${data.main.temp}&deg;F`;
