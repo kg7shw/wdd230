4db7340dbdf6a9abaa9e0066450e893dca8c9b6d
kg7shw <kg7shw@gmail.com>
2023-07-11 14:57:58 -0600

    updated the events image json file name

--- chamber/scripts/events.js
+++ chamber/scripts/events.js
@@ -1,25 +1,42 @@
-fetch("images.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const eventsContainer = document.getElementById("events-container");
-    data.images.forEach((imageUrl) => {
-      const eventDiv = document.createElement("div");
-      eventDiv.classList.add("events");
+const baseURL = "https://kg7shw.github.io/wdd230";
+const eventsImgURL = `${baseURL}/data/eventsImg.json`;
 
-      const picture = document.createElement("picture");
-      const img = document.createElement("img");
-      img.src = imageUrl;
-      img.alt = "Event Image";
-      img.classList.add("event-img");
-      img.loading = "lazy";
+const eventsContainer = document.getElementById("events-container");
 
-      const eventText = document.createElement("p");
-      eventText.textContent = "Lorem ipsum dolor sit amet";
+async function getEventImages() {
+  try {
+    const response = await fetch(eventsURL);
+    if (response.ok) {
+      const data = await response.json();
+      displayEventImages(data);
+    } else {
+      throw new Error(await response.text());
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-      picture.appendChild(img);
-      eventDiv.appendChild(picture);
-      eventDiv.appendChild(eventText);
-      eventsContainer.appendChild(eventDiv);
-    });
-  })
-  .catch((error) => console.error(error));
+function displayEventImages(data) {
+  for (let imageUrl of data.images) {
+    const eventDiv = document.createElement("div");
+    eventDiv.classList.add("events");
+
+    const picture = document.createElement("picture");
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.alt = "Event Image";
+    img.classList.add("event-img");
+    img.loading = "lazy";
+
+    const eventText = document.createElement("p");
+    eventText.textContent = "Lorem ipsum dolor sit amet";
+
+    picture.appendChild(img);
+    eventDiv.appendChild(picture);
+    eventDiv.appendChild(eventText);
+    eventsContainer.appendChild(eventDiv);
+  }
+}
+
+getEventImages();
