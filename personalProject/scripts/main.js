1d3dde2a4e8f18e1c5fcbb0025af0b4c65ca7094
kg7shw <kg7shw@gmail.com>
2023-07-12 22:16:37 -0600

    update

---
+++ personalProject/scripts/main.js
@@ -0,0 +1,42 @@
+const baseURL = "https://kg7shw.github.io/wdd230";
+const cardsURL = `${baseURL}/data/infoCards.json`;
+
+const SectionMainDisplay = document.querySelector(".info-card-section");
+
+async function getCards() {
+  try {
+    const response = await fetch(cardsURL);
+    if (response.ok) {
+      const data = await response.json();
+      displayCards(data.cards);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayCards(cards) {
+  const ul = document.createElement("ul");
+  ul.setAttribute("id", "card-list");
+  ul.classList.add("card-list");
+  ul.classList.add("grid");
+
+  for (let card of cards) {
+    const li = document.createElement("li");
+    li.classList.add("card-list-item");
+    li.innerHTML = `
+      <img src="${card.imageUrl}" width="50">
+      <h2 class="title">${card.title}</h2>
+      <p class="description">${card.description}</p>
+      <a class="website" href="${card.websiteUrl}">${card.websiteUrl}</a>
+    `;
+
+    ul.appendChild(li);
+  }
+
+  SectionMainDisplay.appendChild(ul);
+}
+
+getCards();
